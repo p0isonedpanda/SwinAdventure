45dfce4355debe4f934257fe225c0dcc26c34985
Daniel Coady <daniel.coady@outlook.com.au>
2018-10-25 11:31:59 +1100

    Added locations

--- Player.cs
+++ Player.cs
@@ -5,6 +5,7 @@ namespace SwinAdventure
     public class Player : GameObject, IHaveInventory
     {
         private Inventory _inventory;
+        private Location _currentLocation;
 
         public override string LongDescription
         {
@@ -22,24 +23,45 @@ namespace SwinAdventure
             }
         }
 
+        public Location CurrentLocation
+        {
+            get
+            {
+                return _currentLocation;
+            }
+        }
+
         public Player(string name, string desc) : base (new string[] { "me", "inventory" }, name, desc)
         {
             _inventory = new Inventory();
+            _currentLocation = null;
         }
 
         public GameObject Locate(string id)
         {
+            GameObject output = null;
             switch (id)
             {
                 case "me":
-                    return this;
-                
                 case "inventory":
-                    return this;
-
+                    output = this;
+                    break;
+                
                 default:
-                    return _inventory.Fetch(id);
+                    output = _inventory.Fetch(id);
+                    break;
             }
+
+            if (output == null && _currentLocation != null) output = _currentLocation.LocationInventory.Fetch(id);
+
+            return output;
+        }
+
+        public void EnterLocation(Location newLoc)
+        {
+            if (_currentLocation != null) _currentLocation.Exit();
+            newLoc.Enter(this);
+            _currentLocation = newLoc;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,39 +2,11 @@
 
 namespace SwinAdventure
 {
-    class Program
+    public class Program
     {
-        static Player p;
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to SwinAdventure!");
-            Console.Write("Please enter your name:\n> ");
-            string name = Console.ReadLine();
-            Console.Write("Please describe yourself to me:\n> ");
-            string desc = Console.ReadLine();
-
-            p = new Player(name, desc);
-            Console.WriteLine("Welcome {0}, to the wonderful world of Swinburne!", p.Name);
-            p.PlayerInventory.Put(new Item(new string[] { "phone" }, "Phone", "A smartphone that you hardly use the phone features of"));
-            Console.WriteLine("You picked up a Phone (phone)! Placed in inventory");
-            p.PlayerInventory.Put(new Item(new string[] { "pen" }, "Pen", "A blue ballpoint pen"));
-            Console.WriteLine("You picked up a Pen (pen)! Placed in inventory");
-            Bag b = new Bag(new string[] { "bag" }, "Bag", "It can hold a variety of things");
-            Console.WriteLine("Found a bag!");
-            b.BagInventory.Put(new Item(new string[] { "thonkpad" }, "ThonkPad", "One of the finest laptops to grace this earth"));
-            Console.WriteLine("You picked up a ThonkPad (thonkpad)! Placed in bag");
-            p.PlayerInventory.Put(b);
-            Console.WriteLine("Placed Bag (bag) in inventory");
-
-            string input;
-            LookCommand cmd = new LookCommand();
-            while (true)
-            {
-                Console.Write("> ");
-                input = Console.ReadLine();
-                Console.WriteLine(cmd.Execute(p, input.Split(" ")));
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
